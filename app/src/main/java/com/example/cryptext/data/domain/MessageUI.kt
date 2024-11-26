56993e8d1aa4804aa85a0802b7d8e8b591d9a4cf
marcos-costa <marcoscaafonso@gmail.com>
2024-11-26 11:08:48 -0400

    Remove unnecessary folders

--- app/src/main/java/com/example/cryptext/data/domain/MessageUI.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cryptext.data.domain
-
-data class MessageUI (
-    val friendName: String,
-    val message: String,
-    val date: String,
-    val hour: String,
-    val received: Boolean
-)
--- app/src/main/java/com/example/cryptext/data/domain/UserUI.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cryptext.data.domain
-
-data class UserUI (
-    val name: String,
-    val username: String,
-    val email: String,
-    val status: String = "Online"
-)
